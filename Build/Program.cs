4cb62a589fe83702ac4dfba3d8ce34f45727a291
nathaniel.oakes10 <nathaniel.oakes10@outlook.com>
2020-07-10 11:03:28 -0700

    working final version

--- Build/Program.cs
+++ Build/Program.cs
@@ -5,14 +5,14 @@ namespace Multiples_of_3_and_5
 {
     class Program
     {
-
+        
 
         static void Main(string[] args)
         {
             List<int> correctNumbers = new List<int>();
             int sum = 0;
             LoopforSetNumber(ref correctNumbers);
-            foreach (int i in correctNumbers)
+            foreach(int i in correctNumbers)
             {
                 sum += i;
             }
@@ -21,15 +21,15 @@ namespace Multiples_of_3_and_5
 
         private static void LoopforSetNumber(ref List<int> myList)
         {
-
-            for (int i = 1; i < 1000; i++)
+            
+            for (int i=1; i <1000; i++)
             {
                 if (i % 3 == 0)
                 {
                     myList.Add(i);
                 }
-                if (!myList.Contains(i))
-                {
+                if(!myList.Contains(i))
+                { 
                     if (i % 5 == 0)
                     {
                         myList.Add(i);
