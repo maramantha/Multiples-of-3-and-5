1e582c03f04e620758f00acf292163c16f13e881
nathaniel.oakes10 <nathaniel.oakes10@outlook.com>
2020-07-10 10:48:52 -0700

    Mulitples of 3 and 5

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Multiples_of_3_and_5
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<int> correctNumbers = new List<int>();
+            int sum = 0;
+            LoopforSetNumber(ref correctNumbers);
+            foreach (int i in correctNumbers)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void LoopforSetNumber(ref List<int> myList)
+        {
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    myList.Add(i);
+                }
+                if (!myList.Contains(i))
+                {
+                    if (i % 5 == 0)
+                    {
+                        myList.Add(i);
+                    }
+                }
+            }
+        }
+    }
+
+}
